14ecdfbcffcb9739d34fc886bcfd9f96ec31ed87
dpinezich <david.pinezich@gmail.com>
2019-02-05 21:29:23 +0100

    corrected with heroku factory

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. Something somex")
+    return HttpResponse("Hello, world. You're at the polls index.")
